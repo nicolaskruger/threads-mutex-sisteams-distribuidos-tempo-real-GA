4c1ff99f0467d1f54a464d6b917969e45a378134
nicolas <nicolas.nkruger@gmail.com>
2022-03-30 16:11:31 -0300

    ta e tb done

--- main.c
+++ main.c
@@ -1,25 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
 
-int vector[10];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+bool done = false;
+int vector[10] = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+};
+
+void log(char * mensagem, int temperatura){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("time: %d-%02d-%02d %02d:%02d:%02d | ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("temp: %d | mensagem: ", temperatura);
+    printf(mensagem);
+    printf("\n\r");
+}
 
 void *ta(void *args)
 {
+    pthread_mutex_lock(&mutex);
     for (size_t i = 0; i < 10; i++)
     {
-        vector[i] = rand();
-        printf("%d \n", vector[i]);
+        vector[i] = rand() * 60.0 / RAND_MAX;
+    }
+    done = true;
+    pthread_mutex_unlock(&mutex);
+}
+
+void await(){
+    pthread_mutex_lock(&mutex);
+    bool await = !done;
+    pthread_mutex_unlock(&mutex);
+    while (await)
+    {
+        pthread_mutex_lock(&mutex);
+        bool await = !done;
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
     }
 }
+
+void *tb(void *args)
+{
+    await();
+    pthread_mutex_lock(&mutex);
+    for (size_t i = 0; i < 10; i++)
+    {
+        int temperatura = vector[i];
+        if(temperatura < 20){
+            log("temperatura menor que 20 graus celsios", temperatura);
+        }
+        if(temperatura > 30){
+            log("temperatura maior que 30 graus celsios", temperatura);
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
 int main()
 {
 
-    pthread_t thread_a;
+    pthread_t thread_a, thread_b;
 
     pthread_create( & thread_a, NULL, & ta, NULL );
+    pthread_create( & thread_b, NULL, &tb, NULL);
 
     pthread_join( thread_a, NULL);
+    pthread_join( thread_b, NULL);
 
     exit(0);
 }
